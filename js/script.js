2ace31996a46a19804849c6afe99d9e3a6663cfc
mmuzamil46 <servhost22@gmail.com>
2025-01-08 11:43:17 +0300

    secondCommit

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@
 // function dropdown(){
 //     drop.('drop');
 // }
+let hcj = document.getElementById("hcj");
+let desc = document.getElementById("desc");
+
+function showDesc(element){
+    // console.log(element);
+// element.children[0].classList.toggle('d-none');
+element.children[1].classList.remove('d-none');
+}
+ function hideDesc(element){
+    element.children[1].classList.add('d-none');
+ }
+// hcj.classList.toggle('d-none');
+// desc.classList.toggle('d-none');
+// }
