1162657dc22ce1f9274a2b0112ba396470f61bbc
mmuzamil46 <servhost22@gmail.com>
2025-01-21 15:23:38 +0300

    dropdown added

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@
 // }
 let hcj = document.getElementById("hcj");
 let desc = document.getElementById("desc");
-
+const drop = document.getElementById("drop");
+const buton = document.getElementById("buton");
 function showDesc(element){
     // console.log(element);
 // element.children[0].classList.toggle('d-none');
@@ -14,6 +15,15 @@ element.children[1].classList.remove('d-none');
  function hideDesc(element){
     element.children[1].classList.add('d-none');
  }
-// hcj.classList.toggle('d-none');
-// desc.classList.toggle('d-none');
-// }
+function dropdown(){
+    drop.classList.toggle('d-none');
+if(buton.classList.contains('fa-ellipsis')){
+    buton.classList.remove('fa-ellipsis');
+    buton.classList.add('fa-x');
+    drop.style.opacity = 1;
+}else{
+    buton.classList.remove('fa-x');
+    buton.classList.add('fa-ellipsis');
+    drop.style.opacity = 0;
+}
+}
