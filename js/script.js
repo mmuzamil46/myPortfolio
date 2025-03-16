0933d4121dee46c7106c189b2dcccb4c56e43524
mohammed muzamil <139592511+mmuzamil46@users.noreply.github.com>
2025-03-16 23:41:18 +0300

    Update script.js dead line

--- js/script.js
+++ js/script.js
@@ -1,29 +1,48 @@
-// let drop = document.getElementById("drop");
+$(document).ready(function(){
 
-// function dropdown(){
-//     drop.('drop');
-// }
-let hcj = document.getElementById("hcj");
-let desc = document.getElementById("desc");
-const drop = document.getElementById("drop");
-const buton = document.getElementById("buton");
-function showDesc(element){
-    // console.log(element);
-// element.children[0].classList.toggle('d-none');
-element.children[1].classList.remove('d-none');
-}
- function hideDesc(element){
-    element.children[1].classList.add('d-none');
- }
-function dropdown(){
-    drop.classList.toggle('d-none');
-if(buton.classList.contains('fa-ellipsis')){
-    buton.classList.remove('fa-ellipsis');
-    buton.classList.add('fa-x');
-    drop.style.opacity = 1;
-}else{
-    buton.classList.remove('fa-x');
-    buton.classList.add('fa-ellipsis');
-    
-}
-}
+   // $('.side-bar').hide();
+    //open side bar menu
+    $('.openi').click(function(){
+        $('.side-bar').removeClass('d-none');
+        $('.side-bar').animate({width :"60%"},1000);
+        $('.side-bar').children().slideDown(700);
+    });
+    //close side bar menu when the x icon  clicked 
+
+    $('.closei i').click(function(){
+        $('.side-bar').animate({width :"0%"},700);
+        $('.side-bar').children().slideUp(400);
+    });
+    //close the side bar when link from menu is clicked
+    $('.side-menu li').click(function(){
+        $('.side-bar').animate({width :"0%"},700);
+        $('.side-bar').children().slideUp(400);
+    });
+
+    //hover effect for side bar menu links
+    $('.side-menu li').hover(function(){
+        $(this).css("background-color", "white")
+    },function(){
+        $(this).css("background-color","")
+    });
+
+
+
+//show and hide expertise detail on small screen
+if($(window).width() < 768){
+    $('.exper-content p').hide();
+} 
+
+$(window).resize(function(){
+    if($(window).width() < 768){
+        $('.exper-content p').hide();
+       
+    } else {
+        $('.exper-content p').show();
+    }
+})
+$('.exper-content h2').click(function(){
+    if($(window).width() < 768)
+    $(this).next().slideToggle(500);
+})
+})
